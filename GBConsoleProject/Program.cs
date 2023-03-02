d58f9e03550246a31c46a4fc41aa134cb79c1d87
GB-Comp <101490824@CLC401IT15>
2023-03-02 14:00:25 -0500

    First commit

---
+++ GBConsoleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GBConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
